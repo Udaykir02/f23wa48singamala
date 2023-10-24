57f776aca0b004f2efbc370f0bccb0d16b0d1ff3
Uday Singamala <143046094+Udaykir02@users.noreply.github.com>
2023-10-23 19:41:21 -0500

    final

--- routes/computation.js
+++ routes/computation.js
@@ -2,15 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next)
-{
-    var x = Math.random()
-    var z = Math.random()
-    sin = Math.sin(x);
-    cosh= Math.cosh(x)
-    y = Math.floor(sin);
-    res.send("Math.sin() applied to "+x+" is "+sin +"<br></br>"+"Math.cosh() applied to "+x+" is "+cosh+"<br></br>"+
-                "Math.floor() applied to "+sin+" is "+y);
+router.get('/', function(req, res, next) {
+    var x = Math.random();
+    var z = Math.random();
+    var sin = Math.sin(x);
+    var cosh = Math.cosh(z);
+    var y = Math.floor(sin);
+    
+    res.setHeader('Content-Type', 'text/html'); 
+    res.send("Math.sin() applied to " + x + " is " + sin + "<br><br>" +
+              "Math.cosh() applied to " + z + " is " + cosh + "<br><br>" +
+              "Math.floor() applied to " + sin + " is " + y);
 });
 
 module.exports = router;
